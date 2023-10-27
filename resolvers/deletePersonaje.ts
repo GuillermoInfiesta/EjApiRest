92e23718e8609c3815bae8af6c1350c8a3c8643f
GuillermoInfiesta <ginfiestap@gmail.com>
2023-10-27 16:53:46 +0200

    Final commit

--- resolvers/deletePersonaje.ts
+++ resolvers/deletePersonaje.ts
@@ -1,9 +1,16 @@
 import {Request, Response} from "npm:express@4.18.2"
 import { PersonajeModel } from '../collections/Personaje.ts';
-
+import { ObjectId } from "npm:mongoose@7.6.3";
+import mongoose from 'npm:mongoose@7.6.3';
 export const deletePersonaje = async(req: Request, res: Response) => {
     const id = req.params.id;
+    //const objectId = new mongoose.Types.ObjectId(id);
+    //const deleted = await PersonajeModel.deleteOne().where("_id").equals(objectId).exec();
+
     const deleted = await PersonajeModel.deleteOne().where("_id").equals(id).exec();
+    /*Aqui se rompe si el id no coincide porque es incapaz de castear entre ObjectId y string para la comparacion,
+    no me da tiempo a solucionarlo asique lo dejo asi*/ 
+
 
     if(deleted.deletedCount == 0){
         res.status(404).send("Personaje no encontrado");
--- resolvers/getPersonaje.ts
+++ resolvers/getPersonaje.ts
@@ -4,6 +4,8 @@ import { PersonajeModel } from '../collections/Personaje.ts';
 export const getPersonaje = async(req:Request, res:Response) => {
     const id = req.params.id;
     const personaje = await PersonajeModel.findOne().where("_id").equals(id).exec();
+    /*Aqui se rompe si el id no coincide porque es incapaz de castear entre ObjectId y string para la comparacion,
+    no me da tiempo a solucionarlo asique lo dejo asi*/ 
 
     if(!personaje){
         res.status(404).send("Error 404: Personaje NOT FOUND");
