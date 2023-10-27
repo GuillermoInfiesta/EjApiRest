0655f0b277386ed11ae8e90a10edee9b93edcdb5
GuillermoInfiesta <ginfiestap@gmail.com>
2023-10-27 16:35:57 +0200

    Tercer commit

--- resolvers/deletePersonaje.ts
+++ resolvers/deletePersonaje.ts
@@ -6,7 +6,7 @@ export const deletePersonaje = async(req: Request, res: Response) => {
     const deleted = await PersonajeModel.deleteOne().where("_id").equals(id).exec();
 
     if(deleted.deletedCount == 0){
-        res.status(404).send("Producto no encontrado");
+        res.status(404).send("Personaje no encontrado");
         return;
     }
 
