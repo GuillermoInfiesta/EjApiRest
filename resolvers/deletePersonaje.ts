3e16cf08602de03dbddb9d9784dec899dc01c019
GuillermoInfiesta <ginfiestap@gmail.com>
2023-10-27 17:31:39 +0200

    Codigo Solucionado (dsps de la entrega)

--- resolvers/deletePersonaje.ts
+++ resolvers/deletePersonaje.ts
@@ -3,19 +3,17 @@ import { PersonajeModel } from '../collections/Personaje.ts';
 import { ObjectId } from "npm:mongoose@7.6.3";
 import mongoose from 'npm:mongoose@7.6.3';
 export const deletePersonaje = async(req: Request, res: Response) => {
-    const id = req.params.id;
-    //const objectId = new mongoose.Types.ObjectId(id);
-    //const deleted = await PersonajeModel.deleteOne().where("_id").equals(objectId).exec();
-
-    const deleted = await PersonajeModel.deleteOne().where("_id").equals(id).exec();
-    /*Aqui se rompe si el id no coincide porque es incapaz de castear entre ObjectId y string para la comparacion,
-    no me da tiempo a solucionarlo asique lo dejo asi*/ 
-
-
-    if(deleted.deletedCount == 0){
-        res.status(404).send("Personaje no encontrado");
+    const _id = req.params.id;
+    try{
+        const objectId = new mongoose.Types.ObjectId(_id);
+        const deleted = await PersonajeModel.deleteOne().where("_id").equals(objectId).exec();
+        if(deleted.deletedCount == 0){
+            res.status(404).send("Personaje no encontrado");
+            return;
+        }
+    }catch(e){
+        res.status(500).send("ID no válido");
         return;
     }
-
     res.status(200).send("Personaje eliminado");
 }
--- resolvers/getPersonaje.ts
+++ resolvers/getPersonaje.ts
@@ -1,8 +1,20 @@
 import {Request, Response} from "npm:express@4.18.2"
 import { PersonajeModel } from '../collections/Personaje.ts';
+import mongoose from "npm:mongoose@7.6.3";
 
 export const getPersonaje = async(req:Request, res:Response) => {
     const id = req.params.id;
+    try{
+        const objectId = new mongoose.Types.ObjectId(id);
+        const deleted = await PersonajeModel.deleteOne().where("_id").equals(objectId).exec();
+        if(deleted.deletedCount == 0){
+            res.status(404).send("Personaje no encontrado");
+            return;
+        }
+    }catch(e){
+        res.status(500).send("ID no válido");
+        return;
+    }
     const personaje = await PersonajeModel.findOne().where("_id").equals(id).exec();
     /*Aqui se rompe si el id no coincide porque es incapaz de castear entre ObjectId y string para la comparacion,
     no me da tiempo a solucionarlo asique lo dejo asi*/ 
